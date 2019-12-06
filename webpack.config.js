930c3dba39100dcbb3d6816d6597aead3ee28ce2
ashik112 <md.ashekur@gmail.com>
2019-12-06 21:28:46 +0600

    integrate image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,14 +19,14 @@ module.exports = (env, argv) => {
 			path: path.resolve(__dirname, 'deploy/'),
 			filename: 'ancientbeast.js',
 			// chunkFilename: '[id].chunk.js',
-			publicPath: '/'
+			publicPath: '/',
 		},
 
 		devtool: 'inline-source-map',
 		optimization: {
 			splitChunks: {
-				cacheGroups: {}
-			}
+				cacheGroups: {},
+			},
 		},
 
 		module: {
@@ -36,17 +36,48 @@ module.exports = (env, argv) => {
 				{ test: /p2\.js/, use: ['expose-loader?p2'] },
 				{
 					test: /\.less$/,
-					use: ['style-loader', 'css-loader', 'less-loader']
+					use: ['style-loader', 'css-loader', 'less-loader'],
 				},
 				{
 					test: /\.css$/,
-					use: ['style-loader', 'css-loader']
+					use: ['style-loader', 'css-loader'],
 				},
 				{
 					test: /\.(png|jpg|gif|svg|ogg|ico|cur|woff|woff2)$/,
-					use: ['file-loader']
-				}
-			]
+					use: ['file-loader'],
+				},
+				{
+					test: /\.(png|jpg|gif|svg|ogg|ico|cur|woff|woff2)$/,
+					use: [
+						'file-loader',
+						{
+							loader: 'image-webpack-loader',
+							options: {
+								mozjpeg: {
+									progressive: true,
+									arithmetic: false,
+									quality: 100,
+								},
+								// optipng.enabled: false will disable optipng
+								optipng: {
+									enabled: true,
+								},
+								gifsicle: {
+									interlaced: false,
+									optimizationLevel: 3,
+								},
+								svgo: {
+									plugins: [
+										{
+											removeViewBox: false,
+										},
+									],
+								},
+							},
+						},
+					],
+				},
+			],
 		},
 
 		resolve: {
@@ -55,8 +86,8 @@ module.exports = (env, argv) => {
 				p2: p2,
 				phaser: phaser,
 				assets: path.resolve(__dirname, 'assets/'),
-				modules: path.join(__dirname, 'node_modules')
-			}
+				modules: path.join(__dirname, 'node_modules'),
+			},
 		},
 
 		// Eventually we want to use a version of this.
@@ -83,9 +114,9 @@ module.exports = (env, argv) => {
 				template: path.resolve(__dirname, 'src', 'index.html'),
 				favicon: path.resolve(__dirname, 'assets', 'favicon.png'),
 				worker: variables.worker,
-				analytics: ''
-			})
-		]
+				analytics: '',
+			}),
+		],
 	};
 
 	let production = {
@@ -95,9 +126,9 @@ module.exports = (env, argv) => {
 				template: path.resolve(__dirname, 'src', 'index.html'),
 				favicon: path.resolve(__dirname, 'assets', 'favicon.png'),
 				worker: variables.worker,
-				analytics: variables.analytics
-			})
-		]
+				analytics: variables.analytics,
+			}),
+		],
 	};
 
 	let settings;
